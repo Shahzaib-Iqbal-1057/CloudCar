8f5a549f200d99785ec30d83d9ec1effd8938245
umerrr28 <25100017@lums.edu.pk>
2024-03-15 05:50:52 +0500

    Background color and littles in about us

--- frontend/src/pages/aboutUs.js
+++ frontend/src/pages/aboutUs.js
@@ -138,141 +138,145 @@ function AboutUs() {
       </nav>
 
       {/* Section 2 */}
-      <section className="px-2 py-32 bg-white md:px-0">
-        <div className="container items-center max-w-6xl px-8 mx-auto xl:px-5">
-          <div className="flex flex-wrap items-center sm:-mx-3">
-            <div className="w-full md:w-1/2 md:px-3">
-              <div className="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
-                <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
-                  <span className="block xl:inline">Our</span>
-                  <span className="block text-teal-600 xl:inline">
-                    {" "}
-                    Mission
-                  </span>
-                </h1>
-                <p className="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">
-                  At CloudCar, our mission is simple: to create seamless,
-                  personalized experiences that enhance the driving and
-                  ownership experience for all. We believe that the future of
-                  mobility lies in smart, connected solutions that empower
-                  drivers and passengers alike.
-                </p>
+      <div className="bg-gradient-to-b from-gray-900 via-gray-700 to-black">
 
-                {/* <div className="relative flex flex-col sm:flex-row sm:space-x-4">
-                  <a href="#_" className="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto">
-                    Try It Free
-                    <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 ml-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
-                  </a>
-                  <a href="#_" className="flex items-center px-6 py-3 text-gray-500 bg-gray-100 rounded-md hover:bg-gray-200 hover:text-gray-600">
-                    Learn More
-                  </a>
-                </div> */}
+        <section className="px-2 py-32 md:px-0">
+          <div className="container items-center max-w-6xl px-8 mx-auto xl:px-5">
+            <div className="flex flex-wrap items-center sm:-mx-3">
+              <div className="w-full md:w-1/2 md:px-3">
+                <div className="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
+                  <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl">
+                    <span className="block xl:inline">Our</span>
+                    <span className="block text-teal-600 xl:inline">
+                      {" "}
+                      Mission
+                    </span>
+                  </h1>
+                  <p className="mx-auto text-base text-white sm:max-w-md lg:text-xl md:max-w-3xl">
+                    At CloudCar, our mission is simple: to create seamless,
+                    personalized experiences that enhance the driving and
+                    ownership experience for all. We believe that the future of
+                    mobility lies in smart, connected solutions that empower
+                    drivers and passengers alike.
+                  </p>
+
+                  {/* <div className="relative flex flex-col sm:flex-row sm:space-x-4">
+                    <a href="#_" className="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto">
+                      Try It Free
+                      <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 ml-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
+                    </a>
+                    <a href="#_" className="flex items-center px-6 py-3 text-gray-500 bg-gray-100 rounded-md hover:bg-gray-200 hover:text-gray-600">
+                      Learn More
+                    </a>
+                  </div> */}
+                </div>
+              </div>
+              {/* <div className="w-full md:w-1/2">
+                <div className="w-full h-auto overflow-hidden rounded-md sm:rounded-lg lg:rounded-xl">
+                  <img src={`url(${process.env.PUBLIC_URL}/aboutusimg.jpg)`} alt="Example image" className="object-cover w-full h-full"/>
+                </div>
+              </div> */}
+              <div className="w-full md:w-1/2">
+                <div className="w-full h-auto overflow-hidden rounded-md sm:rounded-lg lg:rounded-xl">
+                  <img
+                    src={`${process.env.PUBLIC_URL}/aboutusimg.jpg`}
+                    alt="Example image"
+                    style={{ height: "400px" }}
+                    className="object-cover w-full"
+                  />
+                </div>
               </div>
             </div>
-            {/* <div className="w-full md:w-1/2">
-              <div className="w-full h-auto overflow-hidden rounded-md sm:rounded-lg lg:rounded-xl">
-                <img src={`url(${process.env.PUBLIC_URL}/aboutusimg.jpg)`} alt="Example image" className="object-cover w-full h-full"/>
-              </div>
-            </div> */}
-            <div className="w-full md:w-1/2">
-              <div className="w-full h-auto overflow-hidden rounded-md sm:rounded-lg lg:rounded-xl">
-                <img
-                  src={`${process.env.PUBLIC_URL}/aboutusimg.jpg`}
-                  alt="Example image"
-                  style={{ height: "400px" }}
-                  className="object-cover w-full"
-                />
-              </div>
+          </div>
+        </section>
+
+        {/* Section 3 */}
+        <section className="w-full pt-7 pb-7 md:pt-20 md:pb-24">
+          <div className="box-border flex flex-col items-center content-center px-8 mx-auto leading-6 text-black border-0 border-gray-300 border-solid md:flex-row max-w-7xl lg:px-16">
+            {/* Image */}
+            <div className="box-border relative w-full max-w-md px-4 mt-5 mb-4 -ml-5 text-center bg-no-repeat bg-contain border-solid md:ml-0 md:mt-0 md:max-w-none lg:mb-0 md:w-1/2 xl:pl-10">
+              <img
+                src="https://cdn.devdojo.com/images/december2020/productivity.png"
+                className="p-2 pl-6 pr-5 xl:pl-16 xl:pr-20"
+                alt="Productivity"
+              />
+            </div>
+
+            {/* Content */}
+            <div className="box-border order-first w-full text-black border-solid md:w-1/2 md:pl-10 md:order-none">
+              <h2 className="m-0 text-white text-xl font-semibold leading-tight border-0 border-gray-300 lg:text-3xl md:text-2xl">
+                Our Values
+              </h2>
+              <p className="pt-4 pb-8 m-0 leading-7 text-white border-0 border-gray-300 sm:pr-12 xl:pr-32 lg:text-lg">
+              Operating with honesty, transparency, and unwavering integrity.
+              </p>
+              <ul className="p-0 m-0 leading-6 border-0 border-gray-300">
+                <li className="box-border relative py-1 pl-0 text-left text-white border-solid">
+                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full">
+                    <span className="text-sm font-bold">✓</span>
+                  </span>{" "}
+                  Pushing the limits of technology and reimagining mobility's future.
+                </li>
+                <li className="box-border relative py-1 pl-0 text-left text-white border-solid">
+                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full">
+                    <span className="text-sm font-bold">✓</span>
+                  </span>{" "}
+                  Delivering top-tier products and services, exceeding expectations.
+                </li>
+                <li className="box-border relative py-1 pl-0 text-left text-white border-solid">
+                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full">
+                    <span className="text-sm font-bold">✓</span>
+                  </span>{" "}
+                  Prioritizing the needs and satisfaction of customers above all else.
+                </li>
+              </ul>
+            </div>
+            {/* End Content */}
+          </div>
+          
+          <div className="box-border flex flex-col items-center content-center px-8 mx-auto mt-2 leading-6 text-black border-0 border-gray-300 border-solid md:mt-20 xl:mt-0 md:flex-row max-w-7xl lg:px-16">
+            {/* Content */}
+            <div className="box-border w-full text-black border-solid md:w-1/2 md:pl-6 xl:pl-32">
+              <h2 className="m-0 text-xl text-white font-semibold leading-tight border-0 border-gray-300 lg:text-3xl md:text-2xl">
+              Join Us on the Journey
+              </h2>
+              <p className="pt-4 pb-8 m-0 leading-7 text-white border-0 border-gray-300 sm:pr-10 lg:text-lg">
+                Join us whether you are.
+              </p>
+              <ul className="p-0 m-0 leading-6 border-0 border-gray-300">
+                <li className="box-border relative py-1 pl-0 text-left text-white border-solid">
+                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full">
+                    <span className="text-sm font-bold">✓</span>
+                  </span>{" "}
+                  A driver
+                </li>
+                <li className="box-border relative py-1 pl-0 text-left text-white border-solid">
+                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full">
+                    <span className="text-sm font-bold">✓</span>
+                  </span>{" "}
+                  A partner
+                </li>
+                <li className="box-border relative py-1 pl-0 text-left text-white border-solid">
+                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full">
+                    <span className="text-sm font-bold">✓</span>
+                  </span>{" "}
+                  Enthusiast of cutting-edge technology
+                </li>
+              </ul>
+            </div>
+            {/* End Content */}
+
+            {/* Image */}
+            <div className="box-border relative w-full max-w-md px-4 mt-10 mb-4 text-center bg-no-repeat bg-contain border-solid md:mt-0 md:max-w-none lg:mb-0 md:w-1/2">
+              <img
+                src="https://cdn.devdojo.com/images/december2020/settings.png"
+                className="pl-4 sm:pr-10 xl:pl-10 lg:pr-32"
+                alt="Settings"
+              />
             </div>
           </div>
-        </div>
-      </section>
-
-      {/* Section 3 */}
-      <section className="w-full bg-white pt-7 pb-7 md:pt-20 md:pb-24">
-        <div className="box-border flex flex-col items-center content-center px-8 mx-auto leading-6 text-black border-0 border-gray-300 border-solid md:flex-row max-w-7xl lg:px-16">
-          {/* Image */}
-          <div className="box-border relative w-full max-w-md px-4 mt-5 mb-4 -ml-5 text-center bg-no-repeat bg-contain border-solid md:ml-0 md:mt-0 md:max-w-none lg:mb-0 md:w-1/2 xl:pl-10">
-            <img
-              src="https://cdn.devdojo.com/images/december2020/productivity.png"
-              className="p-2 pl-6 pr-5 xl:pl-16 xl:pr-20"
-              alt="Productivity"
-            />
-          </div>
-
-          {/* Content */}
-          <div className="box-border order-first w-full text-black border-solid md:w-1/2 md:pl-10 md:order-none">
-            <h2 className="m-0 text-xl font-semibold leading-tight border-0 border-gray-300 lg:text-3xl md:text-2xl">
-              Our Values
-            </h2>
-            <p className="pt-4 pb-8 m-0 leading-7 text-gray-700 border-0 border-gray-300 sm:pr-12 xl:pr-32 lg:text-lg">
-            Operating with honesty, transparency, and unwavering integrity.
-            </p>
-            <ul className="p-0 m-0 leading-6 border-0 border-gray-300">
-              <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
-                <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full">
-                  <span className="text-sm font-bold">✓</span>
-                </span>{" "}
-                Pushing the limits of technology and reimagining mobility's future.
-              </li>
-              <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
-                <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full">
-                  <span className="text-sm font-bold">✓</span>
-                </span>{" "}
-                Delivering top-tier products and services, exceeding expectations.
-              </li>
-              <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
-                <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full">
-                  <span className="text-sm font-bold">✓</span>
-                </span>{" "}
-                Prioritizing the needs and satisfaction of customers above all else.
-              </li>
-            </ul>
-          </div>
-          {/* End Content */}
-        </div>
-        <div className="box-border flex flex-col items-center content-center px-8 mx-auto mt-2 leading-6 text-black border-0 border-gray-300 border-solid md:mt-20 xl:mt-0 md:flex-row max-w-7xl lg:px-16">
-          {/* Content */}
-          <div className="box-border w-full text-black border-solid md:w-1/2 md:pl-6 xl:pl-32">
-            <h2 className="m-0 text-xl font-semibold leading-tight border-0 border-gray-300 lg:text-3xl md:text-2xl">
-            Join Us on the Journey
-            </h2>
-            <p className="pt-4 pb-8 m-0 leading-7 text-gray-700 border-0 border-gray-300 sm:pr-10 lg:text-lg">
-              Join us whether you are.
-            </p>
-            <ul className="p-0 m-0 leading-6 border-0 border-gray-300">
-              <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
-                <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full">
-                  <span className="text-sm font-bold">✓</span>
-                </span>{" "}
-                A driver
-              </li>
-              <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
-                <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full">
-                  <span className="text-sm font-bold">✓</span>
-                </span>{" "}
-                A partner
-              </li>
-              <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
-                <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-300 rounded-full">
-                  <span className="text-sm font-bold">✓</span>
-                </span>{" "}
-                Enthusiast of cutting-edge technology
-              </li>
-            </ul>
-          </div>
-          {/* End Content */}
-
-          {/* Image */}
-          <div className="box-border relative w-full max-w-md px-4 mt-10 mb-4 text-center bg-no-repeat bg-contain border-solid md:mt-0 md:max-w-none lg:mb-0 md:w-1/2">
-            <img
-              src="https://cdn.devdojo.com/images/december2020/settings.png"
-              className="pl-4 sm:pr-10 xl:pl-10 lg:pr-32"
-              alt="Settings"
-            />
-          </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </>
   );
 }
