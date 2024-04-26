1e16aba17deacf1759b7a7a33908056198ae666a
Shahzaib-Iqbal-1057 <shahzaibiqbal1057@gmail.com>
2024-04-26 14:23:54 +0500

    changed fonts

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(cors())
 const server = http.createServer(app)
 const io = new Server(
     server,{cors:{
-        origin:"cloud-car-chi.vercel.app",
+        origin:"localhost:3000",
         methods: ["GET", "POST"]
     },
 })
